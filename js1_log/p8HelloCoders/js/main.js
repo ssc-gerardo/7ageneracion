a7a70aed6e6ccd753bdfaa7dd448c44b99445a3c
ssc-gerardo <gerardo_fl_ssc@outlook.com>
2020-03-04 19:13:32 -0600

    primera clase de js

---
+++ js1_log/p8HelloCoders/js/main.js
@@ -0,0 +1,12 @@
+/*console.log("hola mundos")
+console.warn("aware coders!!")
+console.error("ya valio")
+*/
+var y=5
+var x=6
+console.log("este numero es x =",x)
+console.log("este numero es y=",y)
+console.log("la suma de x+y=",x+y)
+console.log("la resta de x-y",x-y)
+console.log("la divición de x/y",x/y)
+console.log("la multiplicación de x*y",x*y)
